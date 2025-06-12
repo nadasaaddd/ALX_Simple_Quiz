f7c3f734fa79ae8b8cb2e23e9abf15edba3a06a8
Nada Saad <nadasaaddd@gmail.com>
2025-06-13 02:31:25 +0300

    task1

---
+++ quiz.js
@@ -0,0 +1,20 @@
+function checkAnswer() {
+  var correctAnswer = "4";
+  // Retrieve the checked radio button
+  const selectedRadio = document.querySelector('input[name="quiz"]:checked');
+  // Get the user's answer and store it in userAnswer
+  let userAnswer = selectedRadio ? selectedRadio.value : null;
+
+  if (userAnswer == correctAnswer) {
+    document.getElementById("feedback").textContent = "Correct! Well done.";
+  } else {
+    document.getElementById("feedback").textContent =
+      "That's incorrect. Try again!.";
+  }
+}
+
+// Select the submit button
+const submitButton = document.getElementById("submit-answer");
+
+// Add click event listener to the submit button
+submitButton.addEventListener("click", checkAnswer);
